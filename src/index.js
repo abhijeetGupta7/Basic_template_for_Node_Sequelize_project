496a7f59c7ae3be055ca5763d41ce835ac789f01
abhijeetGupta7 <127014104+abhijeetGupta7@users.noreply.github.com>
2024-08-15 23:21:18 +0530

    Basic Node sequelize Project Template

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const apiRouter = require("./routes");
 
 const app=express();
 
-app.use("/api",apiRouter);
+app.use("/api",apiRouter);   // here /api has a meaning, generally this prefix is used when the routes here are actually apis that are made to be used exposed api, if these were routes for serving static files like html pages or someting /home for home page, then in that case /api prefix was not used
 
 app.listen(PORT, (req,res)=>{
     console.log(`Server is listening at ${PORT}`);    
